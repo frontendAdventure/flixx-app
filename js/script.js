5a848a8d845ceed65af9c25aa6c5e039a7b5c43e
frontendAdventure <mgierszanow@yahoo.co.uk>
2023-06-03 12:44:41 +0200

    Popular shows and spinner added

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularTVShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
@@ -40,11 +40,58 @@ async function fetchAPIData(endpoint) {
   const API_KEY = '0bc9de3e7dc77bc172b2b2b61ea07bbe';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
+
   const data = await response.json();
+
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
+
+async function displayPopularTVShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((tvShow) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="tv-details.html?id=${tvShow.id}">
+    ${
+      tvShow.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${tvShow.poster_path}"
+      class="card-img-top"
+      alt="${tvShow.name}"
+    />`
+        : `<img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="${tvShow.name}"
+  />`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${tvShow.name}</h5>
+    <p class="card-text">
+      <small class="text-muted">Aired: ${tvShow.first_air_date}</small>
+    </p>
+  </div>
+    `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
 }
 
 async function displayPopularMovies() {
@@ -76,6 +123,6 @@ alt="${movie.title}"
           </div>
           `;
 
-          document.querySelector('#popular-movies').appendChild(div);
+    document.querySelector('#popular-movies').appendChild(div);
   });
 }
