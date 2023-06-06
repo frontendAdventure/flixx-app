f539d4abdfd259a7ace5fe479c0ff04cdafe33f5
frontendAdventure <mgierszanow@yahoo.co.uk>
2023-06-06 15:56:53 +0200

    Searching added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey:'0bc9de3e7dc77bc172b2b2b61ea07bbe',
+    apiUrl: 'https://api.themoviedb.org/3/'
+  }
 };
 
 function highlightActiveLink() {
@@ -15,7 +25,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      displaySlider()
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
@@ -28,7 +38,7 @@ function init() {
       displayShowDetails();
       break;
     case '/search.html':
-      console.log('Search');
+      search();
       break;
   }
 
@@ -38,8 +48,8 @@ function init() {
 document.addEventListener('DOMContentLoaded', init);
 
 async function fetchAPIData(endpoint) {
-  const API_KEY = '0bc9de3e7dc77bc172b2b2b61ea07bbe';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
@@ -52,6 +62,88 @@ async function fetchAPIData(endpoint) {
   hideSpinner();
 
   return data;
+}
+
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+  if (global.search.term !== '' && global.search.term !== null) {
+    const {results, total_pages, page} = await searchAPIData();
+    if(results.length ===0){
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+    document.querySelector('#search-term').value ='';
+
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
+
+function displaySearchResults(results){
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+${
+  result.poster_path
+    ? `<img
+src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+class="card-img-top"
+alt="${global.search.type === 'movie' ? result.title : result.name}"
+/>`
+    : `<img
+src="images/no-image.jpg"
+class="card-img-top"
+alt="${global.search.type === 'movie' ? result.title : result.name}"
+/>`
+}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_day : result.first_air_date}</small>
+            </p>
+          </div>
+          `;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
+}
+
+//show alert
+
+function showAlert(message, className='error') {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(()=>alertEl.remove(),3000)
 }
 
 function showSpinner() {
@@ -128,8 +220,7 @@ alt="${movie.title}"
   });
 }
 
-
-async function displayMovieDetails(){
+async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
 
   const movie = await fetchAPIData(`movie/${movieId}`);
@@ -139,20 +230,22 @@ async function displayMovieDetails(){
   displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
-  div.innerHTML =`
+  div.innerHTML = `
   <div class="details-top">
   <div>
-  ${movie.poster_path ? 
-    `<img
+  ${
+    movie.poster_path
+      ? `<img
   src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
   class="card-img-top"
   alt="${movie.title}"
-/>` :
-    `<img
+/>`
+      : `<img
       src="images/no-image.jpg"
       class="card-img-top"
       alt="${movie.title}"
-    />`}
+    />`
+  }
   </div>
   <div>
     <h2>${movie.title}</h2>
@@ -165,61 +258,70 @@ async function displayMovieDetails(){
       ${movie.overview}
     </p>
     <h5>Genres</h5>
-    <ul class="list-group">${movie.genres.map(genre=>{
-      return `<li>${genre.name}</li>`
-    }).join('')}
+    <ul class="list-group">${movie.genres
+      .map((genre) => {
+        return `<li>${genre.name}</li>`;
+      })
+      .join('')}
    
     </ul>
-    <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
   </div>
 </div>
 <div class="details-bottom">
   <h2>Movie Info</h2>
   <ul>
-    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-    <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
     <li><span class="text-secondary">Status:</span> ${movie.status}</li>
   </ul>
   <h4>Production Companies</h4>
-  <div class="list-group">${movie.production_companies.map(company=>{
-    return `<span>${company.name}</span>`
-  }).join(', ')}</div>
+  <div class="list-group">${movie.production_companies
+    .map((company) => {
+      return `<span>${company.name}</span>`;
+    })
+    .join(', ')}</div>
 </div>
   `;
 
   document.querySelector('#movie-details').appendChild(div);
-
-
 }
 
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
 
-function displayBackgroundImage(type, backgroundPath){
-const overlayDiv = document.createElement('div');
-overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
-overlayDiv.style.backgroundSize = 'cover';
-overlayDiv.style.backgroundPosition = 'center';
-overlayDiv.style.backgroundRepeat = 'no-repeat';
-overlayDiv.style.height = '100vh';
-overlayDiv.style.width = '100vw';
-overlayDiv.style.position = 'absolute';
-overlayDiv.style.top = '0';
-overlayDiv.style.left = '0';
-overlayDiv.style.zIndex = '-1';
-overlayDiv.style.opacity = '0.1';
-
-if(type === 'movie'){
-document.querySelector('#movie-details').appendChild(overlayDiv);
-}else{
-  document.querySelector('#show-details').appendChild(overlayDiv);
-}
-}
-
-function addCommasToNumber(number){
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',');
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
-async function displayShowDetails(){
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+async function displayShowDetails() {
   const showId = window.location.search.split('=')[1];
 
   const show = await fetchAPIData(`tv/${showId}`);
@@ -229,20 +331,22 @@ async function displayShowDetails(){
   displayBackgroundImage('tv', show.backdrop_path);
 
   const div = document.createElement('div');
-  div.innerHTML =`
+  div.innerHTML = `
   <div class="details-top">
   <div>
-  ${show.poster_path ? 
-    `<img
+  ${
+    show.poster_path
+      ? `<img
   src="https://image.tmdb.org/t/p/w500${show.poster_path}"
   class="card-img-top"
   alt="${show.name}"
-/>` :
-    `<img
+/>`
+      : `<img
       src="images/no-image.jpg"
       class="card-img-top"
       alt="${show.name}"
-    />`}
+    />`
+  }
   </div>
   <div>
     <h2>${show.name}</h2>
@@ -255,43 +359,50 @@ async function displayShowDetails(){
       ${show.overview}
     </p>
     <h5>Genres</h5>
-    <ul class="list-group">${show.genres.map(genre=>{
-      return `<li>${genre.name}</li>`
-    }).join('')}
+    <ul class="list-group">${show.genres
+      .map((genre) => {
+        return `<li>${genre.name}</li>`;
+      })
+      .join('')}
    
     </ul>
-    <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
   </div>
 </div>
 <div class="details-bottom">
   <h2>Show Info</h2>
   <ul>
-    <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
-    <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air.name}</li>
+    <li><span class="text-secondary">Number of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode to Air:</span> ${
+      show.last_episode_to_air.name
+    }</li>
     <li><span class="text-secondary">Status:</span> ${show.status}</li>
   </ul>
   <h4>Production Companies</h4>
-  <div class="list-group">${show.production_companies.map(company=>{
-    return `<span>${company.name}</span>`
-  }).join(', ')}</div>
+  <div class="list-group">${show.production_companies
+    .map((company) => {
+      return `<span>${company.name}</span>`;
+    })
+    .join(', ')}</div>
 </div>
   `;
 
   document.querySelector('#show-details').appendChild(div);
-
-
 }
-
 
 //display slider movies
 
-async function displaySlider(){
-  const {results} = await fetchAPIData('movie/now_playing');
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
   console.log(results);
-  results.forEach((movie)=>{
+  results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('swiper-slide');
-    div.innerHTML=`
+    div.innerHTML = `
     <a href="movie-details.html?id=${movie.id}">
     <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt = "${movie.title}" />
     </a>
@@ -303,29 +414,29 @@ async function displaySlider(){
     document.querySelector('.swiper-wrapper').appendChild(div);
 
     initSwiper();
-  })
+  });
 }
 
-function initSwiper(){
+function initSwiper() {
   const swiper = new Swiper('.swiper', {
-    slidesPerView : 1,
+    slidesPerView: 1,
     spaceBetween: 30,
     freeMode: true,
     loop: true,
     autoplay: {
       delay: 4000,
-      disableOnInteraction : false
+      disableOnInteraction: false,
     },
     breakpoints: {
-      500:{
-        slidesPerView : 2
+      500: {
+        slidesPerView: 2,
       },
-      700:{
-        slidesPerView : 3
+      700: {
+        slidesPerView: 3,
       },
-      1200:{
-        slidesPerView : 4
+      1200: {
+        slidesPerView: 4,
       },
-    }
-  })
+    },
+  });
 }
