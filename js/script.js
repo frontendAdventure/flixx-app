35999ae0c0b40303237fd830925c48dcf680ab15
frontendAdventure <mgierszanow@yahoo.co.uk>
2023-06-06 16:20:32 +0200

    Pagination added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0
   },
   api: {
     apiKey:'0bc9de3e7dc77bc172b2b2b61ea07bbe',
@@ -71,7 +72,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
@@ -87,8 +88,14 @@ async function search() {
   const urlParams = new URLSearchParams(queryString);
   global.search.type = urlParams.get('type');
   global.search.term = urlParams.get('search-term');
+
   if (global.search.term !== '' && global.search.term !== null) {
-    const {results, total_pages, page} = await searchAPIData();
+    const {results, total_pages, page, total_results} = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
     if(results.length ===0){
       showAlert('No results found');
       return;
@@ -104,6 +111,11 @@ async function search() {
 
 
 function displaySearchResults(results){
+  // clear previous results
+  document.querySelector('#search-results').innerHTML='';
+  document.querySelector('#search-results-heading').innerHTML='';
+  document.querySelector('#pagination').innerHTML='';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -131,9 +143,51 @@ alt="${global.search.type === 'movie' ? result.title : result.name}"
           </div>
           `;
 
+
+    document.querySelector('#search-results-heading').innerHTML=` <h2>${results.length} of ${global.search.totalResults} Results for "${global.search.term}"</h2>`
     document.querySelector('#search-results').appendChild(div);
   });
+
+  displayPagination();
 }
+
+function displayPagination(){
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML=`
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  //disble prev button if on first page
+
+  if(global.search.page===1){
+    document.querySelector('#prev').disabled =true;
+  }
+
+  if(global.search.page===global.search.totalPages){
+    document.querySelector('#next').disabled =true;
+  }
+
+  //next page
+
+  document.querySelector('#next').addEventListener('click', async ()=>{
+    global.search.page++;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  })
+
+  document.querySelector('#prev').addEventListener('click', async ()=>{
+    global.search.page--;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  })
+}
+
+
 
 //show alert
 
